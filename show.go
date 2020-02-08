3c42d8502c1789c36d06effbd3ae4c7140b9f5b4
SimonBuckner <simonbuckner@hotmail.com>
2020-02-08 16:39:31 +0000

    Show device hierachy

--- show.go
+++ show.go
@@ -2,23 +2,10 @@ package netfw
 
 import "fmt"
 
-// ShowSite lists out all the devices in the site
-func ShowSite(site *Site) {
-	fmt.Println(site.GetPath())
-	// for _, fw := range site.children {
-	// ShowFirewall(fw)
-	// }
+// ShowDevice lists out all the devices
+func ShowDevice(dev Device) {
+	fmt.Println(dev.GetPath())
+	for _, child := range dev.getChildren() {
+		ShowDevice(child)
+	}
 }
-
-// // ShowFirewall ..
-// func ShowFirewall(fw Device) {
-// 	fmt.Println(fw.GetPath())
-// 	for _, iface := range fw.G .interfaces {
-// 		ShowIface(iface)
-// 	}
-// }
-
-// ShowIface ..
-// func ShowIface(iface *Iface) {
-// 	fmt.Println(iface.GetPath())
-// }
